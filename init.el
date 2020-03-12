9e1d1524951317c05d6473d06786f2d752602ba7
Nicholas J. Kaminski <nkaminsk@ida.org>
2020-03-12 14:25:17 -0400

    evil mode and such

--- init.el
+++ init.el
@@ -8,9 +8,13 @@
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(display-time-mode t)
- '(package-selected-packages (quote (markdown-mode)))
+ '(evil-search-module (quote evil-search))
+ '(package-selected-packages
+   (quote
+    (neotree evil-escape evil racket-mode helm helm-descbinds helm-swoop markdown-mode)))
  '(scroll-bar-mode (quote right))
  '(show-paren-mode t)
+ '(speedbar-show-unknown-files t)
  '(text-mode-hook (quote (turn-on-auto-fill text-mode-hook-identify)))
  '(tool-bar-mode nil))
 '(tooltip-mode nil)
@@ -58,6 +62,7 @@
 ;; set font face
 (set-frame-font "monospace" nil t)
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; General Behavior
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -70,9 +75,6 @@
 
 ;; goto line
 (global-set-key "\M-g" 'goto-line)
-
-;; Set scratch buffer to text mode
-(setq initial-major-mode 'text-mode)
 
 ;; no more dinging
 (setq visible-bell 1)
@@ -89,10 +91,106 @@
 ;; set default column width
 (setq-default fill-column 79)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Browser
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; set default browser to emacs browser
+(setq browse-url-browser-function 'eww-browse-url)
+
+;; set the wrappoing for buffers
+(setq shr-width 80)
+
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Package Manager
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'package)
 (add-to-list 'package-archives
-    '("melpa-stable" . "https://stable.melpa.org/packages/"))
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Helm  
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'helm)
+(require 'helm-config)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line t)
+
+(defun spacemacs//helm-hide-minibuffer-maybe ()
+  "Hide minibuffer in Helm session if we use the header line as input field."
+  (when (with-helm-buffer helm-echo-input-in-header-line)
+    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+      (overlay-put ov 'window (selected-window))
+      (overlay-put ov 'face
+                   (let ((bg-color (face-background 'default nil)))
+                     `(:background ,bg-color :foreground ,bg-color)))
+      (setq-local cursor-type nil))))
+
+
+(add-hook 'helm-minibuffer-set-up-hook
+          'spacemacs//helm-hide-minibuffer-maybe)
+
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+(helm-autoresize-mode 1)
+
+(helm-mode 1)
+
+
+(require 'helm-descbinds)
+(helm-descbinds-mode)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Speedbar
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'sr-speedbar)
+(setq speedbar-use-images nil)
+(global-set-key [f8] 'sr-speedbar-toggle)
+(custom-set-variables '(speedbar-show-unknown-files t))
+ 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Evil Mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'evil)
+;; start evil mode
+(evil-mode 1)
+;; allow the cursor to behave more like emacs cursor
+(setq evil-move-beyond-eol t)
+(setq evil-move-cursor-back nil)
+;; use the evil search mode
+(setq evil-search-module 'evil-search)
+;; add line numbers
+(global-display-line-numbers-mode)
+
+;; enable evil-escape (exit insert mode with fd)
+(require 'evil-escape)
+(evil-escape-mode)
+(setq-default evil-escape-key-sequence "fd")
